a0ec4a47357c563f80b31bc83bdd504531579745
Askarbek <askarbek101@gmail.com>
2024-07-23 14:00:43 -0400

    fix

--- app/api/redis/route.ts
+++ app/api/redis/route.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import RedisService from '@/services/redis-service';
 
-export const runtime = 'edge'
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
