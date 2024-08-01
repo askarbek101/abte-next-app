95427d5ee4d82ccd96c8a8528c91db625a8f6368
Askarbek <askarbek101@gmail.com>
2024-08-01 12:35:41 -0400

    fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,8 @@ import { Form } from '@/app/form';
 import { signIn } from '@/app/auth';
 import { SubmitButton } from '@/app/submit-button';
 
+export const runtime = 'edge';
+
 export default function Login() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
